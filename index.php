b28f73965466d04e823fee1718a47e655286b646
Georgi-Merjanov <katerach18g@gmail.com>
2025-03-20 14:45:27 +0200

    ime

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - WealthWise</title>
+    <title>DigiSpesti</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
